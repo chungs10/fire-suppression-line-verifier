7d6f3578e9357aebf49d5825b08f9c2160efdd71
chungs10 <chungs10@rpi.edu>
2024-03-31 22:19:14 -0400

    Front-End
    Most of the Front-End Figma is redone, except with Analyze/Capture.

--- Front-End/JS/upload.js
+++ Front-End/JS/upload.js
@@ -1,43 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var captureButton = document.getElementById('capture');
-    var analyzeButton = document.getElementById('analyze');
-  
-    captureButton.addEventListener('click', captureImage);
-    analyzeButton.addEventListener('click', analyzeImage);
-  });
-  
-  function captureImage() {
-    var input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*';
-    input.click();
-  
-    input.onchange = function(event) {
+  var uploadButton = document.getElementById('upload');
+  var analyzeButton = document.getElementById('analyze');
+
+  uploadButton.addEventListener('click', uploadImage);
+  analyzeButton.addEventListener('click', analyzeImage);
+});
+
+function uploadImage() {
+  var input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'image/*';
+  input.click();
+
+  input.onchange = function(event) {
       var file = event.target.files[0];
       var reader = new FileReader();
-  
+
       reader.onload = function(e) {
-        var image = new Image();
-        image.src = e.target.result;
-  
-        var box1 = document.querySelector('.box-capture');
-        box1.innerHTML = '';
-        box1.appendChild(image);
+          var image = new Image();
+          image.src = e.target.result;
+
+          var box1 = document.querySelector('.box-capture');
+          box1.innerHTML = ''; // Clear previous content
+          box1.appendChild(image);
       };
-  
+
       reader.readAsDataURL(file);
-    };
-  }
-    
-  function analyzeImage() {
-    var box1 = document.querySelector('.box-capture');
-    var image = box1.querySelector('img');
-    
-    if (image) {
+  };
+}
+  
+function analyzeImage() {
+  var box1 = document.querySelector('.box-capture');
+  var image = box1.querySelector('img');
+  
+  if (image) {
       console.log("Analyzing image:", image.src);
       // No Analysis Code yet
-    } else {
+  } else {
       console.log("No image found to analyze.");
-    }
   }
-  
+}
